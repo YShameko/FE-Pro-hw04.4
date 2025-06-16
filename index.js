60f2a1ac4b41d6859c6cd9650a4efd3b6b9e1399
YShameko <yshameko@gmail.com>
2025-06-16 19:34:37 +0300

    hw04.4(1)

--- index.js
+++ index.js
@@ -15,19 +15,16 @@
 let numOrStr = prompt('input number or string');
 console.log(numOrStr);
 
-switch (numOrStr) {
-    case null:
+switch (true) {
+    case numOrStr === null:
         console.log('ви скасували');
         break;
-    case '':
+    case numOrStr.trim() === '':
         console.log('Empty String');
         break;
-    case isNaN(+numOrStr):
+    case (isNaN(+numOrStr)):
         console.log(' number is Ba_NaN');
         break;
     default:
         console.log('OK!');
 }
-
-
-
